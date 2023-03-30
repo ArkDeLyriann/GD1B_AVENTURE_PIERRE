a264a026a1cee841a05e9dd8d3d66055e3af999c
ChevalierAlexis~ <1hugopierre1@gmail.com>
2023-03-30 12:59:37 +0200

    oui

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import menu from "/src/levels/menuPrincipal.js";
+import global from "/src/levels/global.js";
+import kitchen from "/src/levels/kitchen.js";
+import quarters from "/src/levels/quarters.js";
+import chapel from "/src/levels/chapel.js";
+
+var config = {
+    type: Phaser.AUTO,
+    width: GAME_WIDTH, // largeur en pixels
+    height: GAME_HEIGHT, // hauteur en pixels
+    physics: {
+      // définition des parametres physiques
+      default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
+      arcade: {
+        // parametres du mode arcade
+        
+        debug: true // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
+      }
+    },
+    scene: [selection, niveau1, niveau2, niveau3]
+  };
+  
+  // création et lancement du jeu
+  var game = new Phaser.Game(config);
+  game.scene.start("kitchen");
